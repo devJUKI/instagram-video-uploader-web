24446edc9b35e2bd33a532e629a3efef19985774
devJUKI <49684752+devJUKI@users.noreply.github.com>
2023-01-22 23:49:11 +0200

    Update FacebookCode.jsx

--- DSWebAppVite/src/FacebookCode.jsx
+++ DSWebAppVite/src/FacebookCode.jsx
@@ -7,7 +7,7 @@ function FacebookCode() {
             const queryParams = new URLSearchParams(location.search);
             if (queryParams.has('code')) {
                 let code = queryParams.get('code')
-                let response = await axios.post('https://graph.facebook.com/v15.0/oauth/access_token?client_id=483341770598169&redirect_uri=https://localhost:5173/facebook-token/&client_secret=656b89799d8db8fa7eb4d18d6f8bf587&code=' + code)
+                let response = await axios.post('https://graph.facebook.com/v15.0/oauth/access_token?client_id=XXXXXXX&redirect_uri=https://localhost:5173/facebook-token/&client_secret=XXXXXXX&code=' + code)
                 localStorage.setItem('fb_token', response.data['access_token'])
                 window.location.href = '/';
             }
